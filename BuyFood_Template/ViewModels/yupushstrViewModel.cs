425cbc797a822f654642a843973ee09aed256c28
yuccc <a0918426061@gmail.com>
2021-04-07 11:01:49 +0800

    終初版更新

---
+++ BuyFood_Template/ViewModels/yupushstrViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuyFood_Template.ViewModels
+{
+    public class yupushstrViewModel
+    {
+            public string strmember { get; set; }
+    }
+}
