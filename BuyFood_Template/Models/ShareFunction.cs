f941d0294537348ca534773007d9db3c1193e14d
alwaysCwC <always0537@gmail.com>
2021-04-08 12:04:21 +0800

    cwc

---
+++ BuyFood_Template/Models/ShareFunction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyFood_Template.Models
+{
+    public class ShareFunction
+    {
+        public string 產生亂數(int length)
+        {
+            var str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            var next = new Random();
+            var builder = new StringBuilder();
+            for (var i = 0; i < length; i++)
+            {
+                builder.Append(str[next.Next(0, str.Length)]);
+            }
+            return builder.ToString();
+        }
+
+        public void sendEmail(string mailtoAddress,string mailtoName, string subject, string body)
+        {
+            var fromAddress = new MailAddress("sunfengmsit129@gmail.com", "擺腹buyfood");
+            var toAddress = new MailAddress(mailtoAddress, mailtoName);
+            const string fromPassword = "@MSIT129";
+            //const string subject = "Subject";
+            //const string body = "Body";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+
+    }
+}
