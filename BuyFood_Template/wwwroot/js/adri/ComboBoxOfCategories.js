d5c2b3dc1505c8f279ad542e0d067b98b9c67250
alwaysCwC <always0537@gmail.com>
2021-04-09 11:30:03 +0800

    2021 0409 1130 搬運JS

--- BuyFood_Template/wwwroot/js/adri/ComboBoxOfCategories.js
+++ BuyFood_Template/wwwroot/js/adri/ComboBoxOfCategories.js
@@ -1,13 +1,16 @@
-﻿var text = "<select id='SearchOptionCategory' name='sfsname' class='hero__search__form' style='width:10%;height: 45.49px;'>" + "<option value='全部'>全部</option>";
+﻿
+
+var text = "<select id='SearchOptionCategory' name='sfsname' class='hero__search__form' style='width:10%;height: 45.49px;'>" + "<option value='全部'>全部</option>";
 $.ajax({
     url: "/HomePage/getCategory",
     type: "POST",
     success: function (data) {
+        console.log(data);
         for (var i = 0; i < data.length; i++) {
             var catName = data[i].replace(/\s*/g, "");
             text += '<option value="' + catName + '">' + catName + '</option>';
         }
-        text +='</select>';
+        text += '</select>';
         $("div.hero__search__categories").prepend(text);
     }
 });
